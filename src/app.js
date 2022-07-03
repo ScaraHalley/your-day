fb24823c6472bc9a7fb62dfcdbfd51c11614fe46
ScaraHalley <106163549+ScaraHalley@users.noreply.github.com>
2022-07-04 08:52:12 +1000

    fixed collumn glitch

--- src/app.js
+++ src/app.js
@@ -56,6 +56,16 @@ function handleSubmit(event) {
   search(cityInputElement.value);
 }
 
-search("New York");
+function displayFahrenheitTemperature(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  let fahrenheitTemperature = (temperatureElement.innerHTML * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+search("Brisbane");
 let form = document.querySelector("search-form");
 form.addEventListener("submit", handleSubmit);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
