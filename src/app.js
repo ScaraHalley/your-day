eaf53fda62fd72ed6af1c18f3e6233d58db9b535
ScaraHalley <106163549+ScaraHalley@users.noreply.github.com>
2022-07-05 21:39:56 +1000

    forecast js complete

--- src/app.js
+++ src/app.js
@@ -20,26 +20,53 @@ function formatDate(timestamp) {
   let day = days[date.getDay()];
   return ` ${day} ${hours}:${minutes}`;
 }
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
+
   let forecastHTML = `<div class ="row">`;
-  let days = ["thu", "fri", "sat", "sun"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `  
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `  
   <div class="col-2">
-     <div class="weather-forecast-date">${day}</div>
-       <img src="http://openweathermap.org/img/wn/50d@2x.png"
+     <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+     
+       <img src="http://openweathermap.org/img/wn/${
+         forecastDay.weather[0].icon
+       }@2x.png"
         alt=""
-        width="38">
+        width="42">
         <div class="weather-forecast-temperatures">
-         <span class="weather-forecast-high">22°C</span> |
-         <span class="weather-forecast-low">15°C</span>
+         <span class="weather-forecast-high">${Math.round(
+           forecastDay.temp.max
+         )}°C</span> |
+         <span class="weather-forecast-low">${Math.round(
+           forecastDay.temp.min
+         )}°C</span>
      </div>
    </div>
    `;
+    }
   });
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
